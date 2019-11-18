29030cd04d71587dda12eb656f4f3f102b5671b1
CodeMe-IOS <codeme-ios@quanganh-2.local>
2019-11-18 22:52:17 +0700

    Initial Commit

---
+++ Baitap1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baitap1
+//
+//  Created by CodeMe-IOS on 11/18/19.
+//  Copyright © 2019 CodeMe-IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
